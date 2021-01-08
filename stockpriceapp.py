3a6ed76d27324d5c26409a9e552dd16c63754a61
le-kag <76808956+le-kag@users.noreply.github.com>
2021-01-08 20:35:23 +0100

    Add files via upload

---
+++ stockpriceapp.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Getting Stock Price
+
+You'll find below the stock closing price and volume of TESLA.
+
+""")
+
+tickerSymbol = 'TSLA'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='1d', start='2011-1-8', end='2021-1-8')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
